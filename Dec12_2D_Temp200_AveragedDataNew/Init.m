7b8a8cf751ca0327b98640ab0de9d99353389e46
Valentina Mazzotti <valimzztt@gmail.com>
2025-01-12 12:37:00 -0500

    Removed .dat files from folder

--- Dec12_2D_Temp200_AveragedDataNew/Init.m
+++
@@ -1,8 +0,0 @@
-tl = (1:1000)';     % Time plotted range 
-fl = (1:90)';      % Frequency plotted range
-ffl = (5:41)';    % Frequency fitted range
-tpl = 1:1000;         % Time plotted range
-
-Averages = 1;
-LockIn_1 = 1;
-LockIn_2 = 1;
