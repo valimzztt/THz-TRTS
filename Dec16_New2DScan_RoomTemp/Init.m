994ea1c1b99ffa0c8cdf7b97e306beca907a3707
Valentina Mazzotti <valimzztt@gmail.com>
2025-01-12 12:33:23 -0500

    Cached data folders

--- Dec16_New2DScan_RoomTemp/Init.m
+++
@@ -1,8 +0,0 @@
-tl = (1:250)';     % Time plotted range 
-fl = (1:90)';      % Frequency plotted range
-ffl = (5:41)';    % Frequency fitted range
-tpl = 1:40;         % Time plotted range
-
-Averages = 3;
-LockIn_1 = 1;
-LockIn_2 = 1;
